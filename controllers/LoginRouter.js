418275b126a72b6d6b57b205e8fcac3014f5d448
Naslanas <naslava00@gmail.com>
2024-02-13 16:15:28 +0530

    password encrypted

--- controllers/LoginRouter.js
+++ controllers/LoginRouter.js
@@ -1,15 +1,28 @@
 const express=require("express")
 const LoginModel = require("../Model/LoginModel")
-
 const router=express.Router()
 
+const bcrypt=require("bcryptjs")
+
+hashPasswordGenerator = async(pass)=>{
+    const salt=await bcrypt.genSalt(10)
+    return bcrypt.hash(pass,salt)
+}
+    
+
+
 router.post("/register",async(req,res)=>{
-    let data=req.body
-    let blog=new LoginModel(data)
+    let {data}={"data":req.body}
+    let password=req.body.password
+    const hashedPassword=await hashPasswordGenerator(password)
+    data.password=hashedPassword
+    let blog=await LoginModel(data)
     let result=await blog.save()
-    res.json({
-        status:"success"
-    })
+    res.json(
+        {
+            status:"success"
+        }
+    )
 })
 
 module.exports=router
